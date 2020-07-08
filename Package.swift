c42e33845d8d1b8af31055e6c18366f973f3c960
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-08 21:32:52 +0200

    Updated version number in Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "vim",
-            url: "https://github.com/holzschu/vim/releases/download/1.0/vim.xcframework.zip",
+            url: "https://github.com/holzschu/vim/releases/download/ios_1.0/vim.xcframework.zip",
             checksum: "d3a49326ff78e05b9084cf881589734158e9059562f584f88d1de5704ab32add"
         )
     ]
