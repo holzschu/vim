bf67c9473941a16bd42773e03a76db32217d914f
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-08 21:27:02 +0200

    Added Package.swift, for Swift Binary Packages

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "vim",
+    products: [
+        .library(name: "vim", targets: ["vim"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "vim",
+            url: "https://github.com/holzschu/vim/releases/download/1.0/vim.xcframework.zip",
+            checksum: "d3a49326ff78e05b9084cf881589734158e9059562f584f88d1de5704ab32add"
+        )
+    ]
+)
